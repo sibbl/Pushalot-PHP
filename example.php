8ecd149bb558b92504244e8e2398366dedc7dc11
Sebastian Mueller <sebastian.mueller@s2011.tu-chemnitz.de>
2013-03-11 22:02:31 +0100

    Initial commit

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+include('pushalot_api.php');
+$pushalot = new Pushalot('35b9832daffc4793aa477e44c0b0910f');
+//$pushalot->setProxy('http://localhost:12345','user:pass');
+$success = $pushalot->sendMessage(array(
+	'Title'=>'Hello world!',
+	'Body'=>"This is a test message!\n\nSent by Pushalot",
+	'LinkTitle'=>'Pushalot.com',
+	'Link'=>'http://www.pushalot.com',
+	'IsImportant'=>true,
+	'IsSilent'=>false
+));
+echo $success?'The message was submitted.':$pushalot->getError();
+?>
